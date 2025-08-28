056dc99b29bfe42e52e2dc68e8a84e7a5d6f1d3a
emrah-ucanokul <emrah@ucanokul.com>
2025-08-28 08:59:23 +0300

    mssql to postgre transfer

--- Data/Repository.cs
+++ Data/Repository.cs
@@ -36,7 +36,7 @@ namespace Ophelia.Data
                     tracker?.OnBeforeInsertEntity(runBeforeUpdateProcesseses);
 
                     effectedRowCount = this.Context.CreateInsertQuery(entity).Execute<int>();
-                    if (this.Context.Connection.Type == DatabaseType.MySQL || this.Context.Connection.Type == DatabaseType.SQLServer)
+                    if (this.Context.Connection.Type == DatabaseType.MySQL || this.Context.Connection.Type == DatabaseType.SQLServer || (this.Context.Connection.Type == DatabaseType.PostgreSQL && this.Context.Configuration.DBIncrementedIdentityColumn))
                     {
                         var pkMethod = Extensions.GetPrimaryKeyProperty(entity.GetType());
                         pkMethod.SetValue(entity, effectedRowCount);
